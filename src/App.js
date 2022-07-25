06c58724a3b7277c4eced58090c18cbdd2131b16
droid <msdroid626@gmail.com>
2022-07-25 13:58:26 +0530

    google login done

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ color:${({theme}) => theme.text};
 
 
 export default function App() {
-  const [ darkMode, setDarkMode] = useState(true);
+  const [ darkMode, setDarkMode] = useState(false);
 
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
